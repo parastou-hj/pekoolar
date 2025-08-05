db63681b437a0be14bf81184184279b31fe56d65
parastou-hj <jalilian.parastou91@gmail.com>
2025-08-05 15:42:45 +0330

    most-sale

--- script.js
+++ script.js
@@ -308,3 +308,25 @@ $(document).ready(function(){
                 articlesCarousel.trigger('next.owl.carousel');
             });
         });
+
+         $(document).ready(function(){
+            $('.owl-carousel').owlCarousel({
+                rtl: true, 
+                loop: true, 
+                // margin: 20, 
+                nav: true, 
+                dots: true, 
+                navText: [">", "<"], 
+                responsive:{
+                    0:{
+                        items:1 
+                    },
+                    600:{
+                        items:2 
+                    },
+                    1000:{
+                        items:5
+                    }
+                }
+            });
+        });
